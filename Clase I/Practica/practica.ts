cce61520f7ea4c20cd036b5dac3e184f7a6edf0b
Federico Gomez Conde <fedeic8@gmail.com>
2023-06-06 11:25:00 -0300

    fibonacci exercise

---
+++ Clase I/Practica/practica.ts
@@ -0,0 +1,18 @@
+//fibonacci
+let prev1:number = 1;
+let prev2:number = 0;
+let fibo:number = 0;
+
+for (let n = 0; n < 15; n++) {
+    if (n <= 1) {
+        fibo = n
+        console.log(fibo);
+    } else {
+        fibo = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = fibo;
+
+        console.log(fibo);
+    }
+    
+}
